4e0c6d282fc26e3f82198909cdf2f706aaced437
Deepa231993 <deepashreeb93@gmail.com>
2025-12-04 15:21:07 +0530

    Add files via upload

---
+++ Python/Activities/activity1.py
@@ -0,0 +1,5 @@
+print("Program enter their name and their age")
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+year = str( ( 2025 - age ) + 100 )
+print(name + " will be 100 years old in the year " + year)
